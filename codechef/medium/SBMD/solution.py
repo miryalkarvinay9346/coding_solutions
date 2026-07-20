d86058db41052a44a319b51fedea2f146c429560
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-20 19:44:13 +0530

    ✅ SBMD | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-20

--- codechef/medium/SBMD/solution.py
+++ codechef/medium/SBMD/solution.py
@@ -3,15 +3,12 @@ n,k=map(int,input().split())
 a=list(map(int,input().split()))
 v=[]
 p=n-k+1
-while p>0:
-    for i in range(n):
-        b=a[i:i+k]
-        b.sort(reverse=True)
-        if len(b)%2==0:
-            s=(b[len(b)//2]+b[(len(b)//2)+1])//2
-            v.append(s)
-
-        else:
-            v.append(b[(len(b))//2])
-    p=p-1    
+for i in range(p):
+    b=a[i:i+k]
+    b.sort()
+    if len(b)%2==0:
+        s=b[(len(b)//2)-1]
+        v.append(s)
+    else:
+        v.append(b[(len(b))//2])
 print(v)
