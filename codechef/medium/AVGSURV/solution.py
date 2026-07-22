c6da313f992c6ffc4261b35f1a908cab98c4b184
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-22 22:12:31 +0530

    ✅ AVGSURV | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-22

--- codechef/medium/AVGSURV/solution.py
+++ codechef/medium/AVGSURV/solution.py
@@ -1,5 +1,9 @@
 # cook your dish here
+import math
 for _ in range(int(input())):
     n=int(input())
     a=list(map(int,input().split()))
-    
+    for i in range(n):
+        k=sum(a)/len(a)
+        
+    print(k)
