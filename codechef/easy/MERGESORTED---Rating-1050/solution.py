6c1f646590b24f9f9128e54d23e9fab026a99b6e
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-25 11:46:30 +0530

    ✅ MERGESORTED - Rating 1050 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-25

---
+++ codechef/easy/MERGESORTED---Rating-1050/solution.py
@@ -0,0 +1,9 @@
+# cook your dish here
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+for i in b :
+    a.append(i)
+a.sort()
+for i in a:
+    print(i,end=" ")
