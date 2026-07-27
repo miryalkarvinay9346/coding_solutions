020a9d916f065cf5853f2501dcfc2fc0ad6a111d
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-27 19:58:08 +0530

    ✅ ARCO | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-27

--- codechef/medium/ARCO/solution.py
+++ codechef/medium/ARCO/solution.py
@@ -1,10 +1,3 @@
 # cook your dish here
 n=int(input())
 a=list(map(int,input().split()))
-stack=[]
-for i in a:
-    if stack>0 and stack[-1]==i:
-        stack.pop()
-    else:
-        stack.append(i)
-print(len(stack))
