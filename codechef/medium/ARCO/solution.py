2d6927764d9d89b657c4b95238ce15f38cbe48ab
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-28 22:22:29 +0530

    ✅ ARCO | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-27

--- codechef/medium/ARCO/solution.py
+++ codechef/medium/ARCO/solution.py
@@ -1,10 +1,3 @@
 # cook your dish here
 n=int(input())
 a=list(map(int,input().split()))
-k=[]
-for i in a:
-    if k==True and k[-1]==i:
-        k.pop()
-    else:
-        k.append(i)
-print(len(k))
