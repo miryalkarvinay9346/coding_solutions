70ec70abf2a46c0df2c77d776de3ccb57d08b97c
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-29 21:37:13 +0530

    ✅ UPDWPAL | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/UPDWPAL/solution.py
+++ codechef/medium/UPDWPAL/solution.py
@@ -2,9 +2,4 @@
 for _ in range(int(input())):
     n=int(input())
     a=list(map(int,input().split()))
-    for i in range(1,n):
-        if a[i]<=x:
-            a[i]=a[i]+1
-        else:
-            a[i]=a[i]-1
-        
+    
