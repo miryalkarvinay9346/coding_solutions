ee3a469cbd3a9c8c66cc068e8ab796613e84ba51
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-29 21:43:11 +0530

    ✅ PRDEXC | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/PRDEXC/solution.py
+++ codechef/medium/PRDEXC/solution.py
@@ -1,14 +1,4 @@
 # cook your dish here
 for _ in range(int(input())):
     x,y,p=map(int,input().split())
-    c=0
-    if x*y>=p:
-        print(0)
-    else:
-        while x*y<p:
-            if min(x,y)==x:
-                x=x+1
-            else:
-                y=y+1
-            c+=1
-        print(c)
+    
