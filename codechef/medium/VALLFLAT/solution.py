a7b201cbdeaafdcd1076cbd9b2a30aa589262ac7
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-29 22:09:10 +0530

    ✅ VALLFLAT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/VALLFLAT/solution.py
+++ codechef/medium/VALLFLAT/solution.py
@@ -3,6 +3,7 @@ for _ in range(int(input())):
     n=int(input())
     a=list(map(int,input().split()))
     c=0
+    m=
     for i in range(n):
         for j in range(n):
             if a[i]!=a[j] and i!=j:
