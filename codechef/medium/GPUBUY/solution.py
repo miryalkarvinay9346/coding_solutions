aa150463638c4da18e79c88c018a8ad589cc6a00
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-29 21:10:08 +0530

    ✅ GPUBUY | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/GPUBUY/solution.py
+++ codechef/medium/GPUBUY/solution.py
@@ -2,7 +2,19 @@
 for _ in range(int(input())):
     x,y,z=map(int,input().split())
     c=0
+    """
+    if y>=z:
+        if z>=x+y:
+            print(1)
+        else:
+            print(-1)
+        continue
+        """
     for i in range(1,101):
         x=x+y
         c=c+z
-        if 
+        if x<=c:
+            print(i)
+            break
+        else:
+            print(-1)
