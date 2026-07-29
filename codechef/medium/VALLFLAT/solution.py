42579970d7a22f191f4d1924acc1726c1a92bffc
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-29 22:11:07 +0530

    ✅ VALLFLAT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/VALLFLAT/solution.py
+++ codechef/medium/VALLFLAT/solution.py
@@ -3,11 +3,7 @@ for _ in range(int(input())):
     n=int(input())
     a=list(map(int,input().split()))
     c=0
-    m=
     for i in range(n):
         for j in range(n):
             if a[i]!=a[j] and i!=j:
-                if a[i]<min(a[i-1],a[i+1]):
-                    a[i-1]=a[i]
-                    a[i+1]=a[i]
-                    
+                if 
