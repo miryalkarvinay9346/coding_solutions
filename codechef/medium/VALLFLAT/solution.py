1478910be55ca7d0ba2bc5e58aa65474aa7a958c
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-29 22:05:10 +0530

    ✅ VALLFLAT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/VALLFLAT/solution.py
+++ codechef/medium/VALLFLAT/solution.py
@@ -6,7 +6,4 @@ for _ in range(int(input())):
     for i in range(n):
         for j in range(n):
             if a[i]!=a[j] and i!=j:
-                if a[i]<min(a[i-1],a[i+1]):
-                    a[i-1]=a[i]
-                    a[i+1]=a[i]
-                    
+                if 
