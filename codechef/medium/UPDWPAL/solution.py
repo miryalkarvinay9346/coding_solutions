93f9d3e81a44d11861912b57e8862960fdad33e4
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-29 21:45:07 +0530

    ✅ UPDWPAL | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/UPDWPAL/solution.py
+++ codechef/medium/UPDWPAL/solution.py
@@ -2,17 +2,9 @@
 for _ in range(int(input())):
     n=int(input())
     a=list(map(int,input().split()))
-    c=0
-    x=a[-1]
     for i in range(1,n):
         if a[i]<=x:
             a[i]=a[i]+1
         else:
             a[i]=a[i]-1
-        if a==a[::-1]:
-            c=1
-            break
-    if c:
-        print("YES")
-    else:
-        print("NO")
+        
