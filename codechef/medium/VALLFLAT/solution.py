0ee588ab1e40177c9eb7f9f667107e92ecd347c2
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-07-29 22:01:44 +0530

    ✅ VALLFLAT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/VALLFLAT/solution.py
+++ codechef/medium/VALLFLAT/solution.py
@@ -1,1 +1,9 @@
 # cook your dish here
+for _ in range(int(input())):
+    n=int(input())
+    a=list(map(int,input().split()))
+    c=0
+    for i in range(n):
+        for j in range(n):
+            if a[i]!=a[j] and i!=j:
+                
