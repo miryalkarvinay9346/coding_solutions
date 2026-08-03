7081806bf8acbea1c6b8001061c147ec28d36cd5
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-03 19:25:57 +0530

    ✅ BAIN | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/medium/BAIN/solution.py
+++ codechef/medium/BAIN/solution.py
@@ -1,2 +1,14 @@
 # cook your dish here
 a,b,c,d=map(int,input().split())
+s=0
+if a==0 or b==0 or c==0 or d==0:
+    print("YES")
+elif a+b==0 or b+c==0 or c+d==0 or d+a==0 or a+c==0 or a+d==0 or b+d==0:
+    print("YES")
+elif a+b+c==0 or a+b+d==0 or b+c+d==0 or c+d+a==0:
+    print("YES")
+elif a+b+c+d==0:
+    print("YES")
+else:
+    print("NO")
+    
