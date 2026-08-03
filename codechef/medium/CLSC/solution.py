fbb3baaa61cec01da98f297e480ac9cad7b51d60
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-03 19:46:07 +0530

    ✅ CLSC | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/medium/CLSC/solution.py
+++ codechef/medium/CLSC/solution.py
@@ -1,9 +1,3 @@
 # cook your dish here
 n=int(input())
 a=list(map(int,input().split()))
-a.sort()
-m=float('inf')
-for i in range(n-1):
-    if abs(a[i]-a[i+1])<m:
-        m=abs(a[i]-a[i+1])
-print(m)
