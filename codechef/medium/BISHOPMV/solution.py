e0db8bac914a5f6ec288cbdaab6d6afb7955089c
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-05 20:44:06 +0530

    ✅ BISHOPMV | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/BISHOPMV/solution.py
+++ codechef/medium/BISHOPMV/solution.py
@@ -1,6 +1,6 @@
 # cook your dish here
 for _ in range(int(input())):
-    x1,x2,y1,y2=map(int,input().split())
+    x1,y1,x2,y2=map(int,input().split())
     if x1==x2 and y1==y2:
         print(0)
     elif abs(x1-x2)==abs(y1-y2):
