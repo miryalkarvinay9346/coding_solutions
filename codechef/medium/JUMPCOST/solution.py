e70d83c3b892f0593e442661901f13b78b2cccc8
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-05 20:56:36 +0530

    ✅ JUMPCOST | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/JUMPCOST/solution.py
+++ codechef/medium/JUMPCOST/solution.py
@@ -2,4 +2,8 @@
 for _ in range(int(input())):
     n=int(input())
     a=list(map(int,input().split()))
-    
+    b=0
+    for i in range(n):
+        for j in range(i,n):
+            b=a[j]-j+i
+    print(b)
