5349c7e7a893ec1cfff2b50edb8f249d3412cbd6
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-12 20:46:42 +0530

    ✅ MUL123 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/MUL123/solution.py
+++ codechef/medium/MUL123/solution.py
@@ -1,17 +1,4 @@
 # cook your dish here
 for _ in range(int(input())):
     n=int(input())
-    c=0
-    while True:
-        if n%3==0:
-            print(c)
-            break
-        
-        if (n+1)%3==0:
-            c+=1
-            print(c)
-            break
-        else:
-            n=round(n/2)*5
-            #print(n)
-            c+=1
+    
