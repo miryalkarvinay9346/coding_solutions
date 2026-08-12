a49ccfd7bbeae00f4a3f3f63d6ae75c214479749
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-12 21:27:57 +0530

    ✅ BUSROW | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/BUSROW/solution.py
+++ codechef/medium/BUSROW/solution.py
@@ -4,7 +4,8 @@ for  _ in range(int(input())):
     f=0
     b=n
     c=0
-    
     for i in range(1,n+1):
-        if (i-1)*m+1>=x and i*m<=m :
+        if (i-1)*m+1<=x and i*m>=x :
+            c=m
+    print(min(c,n-c+1))
             
