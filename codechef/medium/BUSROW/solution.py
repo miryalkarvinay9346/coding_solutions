de448cde44df8c56bd3fd494936aefd3a750ec90
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-12 21:22:22 +0530

    ✅ BUSROW | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/BUSROW/solution.py
+++ codechef/medium/BUSROW/solution.py
@@ -1,4 +1,8 @@
 # cook your dish here
 for  _ in range(int(input())):
     n,m,x=map(int,input().split())
-    
+    f=0
+    b=n
+    for i in range(1,n+1):
+        if (i-1)*m+1>=x and i*m<=m :
+            
