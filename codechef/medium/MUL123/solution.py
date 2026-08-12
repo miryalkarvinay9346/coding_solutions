4e7b1aafc3292ca54bf0d8551dd1a7a71843280d
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-12 20:48:42 +0530

    ✅ MUL123 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/MUL123/solution.py
+++ codechef/medium/MUL123/solution.py
@@ -1,4 +1,17 @@
 # cook your dish here
 for _ in range(int(input())):
     n=int(input())
-    
+    c=0
+    while True:
+        if n%3==0:
+            print(c)
+            break
+        
+        if (n+1)%3==0:
+            c+=1
+            print(c)
+            break
+        else:
+            n=((n//2)+1)*5
+            #print(n)
+            c+=1
