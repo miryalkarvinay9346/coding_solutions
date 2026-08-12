db1ca703e6f6c999a17ee51781e19e31ff00257f
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-12 21:20:43 +0530

    ✅ MUL123 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/MUL123/solution.py
+++ codechef/medium/MUL123/solution.py
@@ -2,11 +2,11 @@
 for _ in range(int(input())):
     n=int(input())
     c=0
-    """
-    while n%3!=0:
+    while True:
         if n%3==0:
             print(c)
             break
+        
         if (n+1)%3==0:
             c+=1
             print(c)
@@ -15,13 +15,3 @@ for _ in range(int(input())):
             n=((n//2)+1)*5
             #print(n)
             c+=1
-            """
-    while n%3!=0:
-        if (n+1)%3==0:
-            c+=1
-            n+=1
-        else:
-            n=((n//2)+1)*5
-            c+=1
-    print(c)
-    
