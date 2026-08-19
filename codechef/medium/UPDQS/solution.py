e3539754e513c45591b720ce76bc1bce51538807
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-19 23:06:08 +0530

    ✅ UPDQS | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/UPDQS/solution.py
+++ codechef/medium/UPDQS/solution.py
@@ -2,16 +2,4 @@
 for _ in range(int(input())):
     n,q=map(int,input().split())
     a=list(map(int,input().split()))
-    i=[]
-    x=[]
-    for m in range(q):
-        v,y=map(int,input().split())
-        i.append(v)
-        x.append(y)
-    for k in range(n-1):
-        s=0
-        s+=a[i+1]
-        s+=a[i-1]
-        s+=a[i]
-        a[i]=min(s,a[i])
-    print(sum(a))
+    
