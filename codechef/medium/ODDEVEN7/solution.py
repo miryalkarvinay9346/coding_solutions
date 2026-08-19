10639f38c5453e6e2154f0f3289f5165b2930ebd
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-19 21:26:02 +0530

    ✅ ODDEVEN7 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/ODDEVEN7/solution.py
+++ codechef/medium/ODDEVEN7/solution.py
@@ -2,4 +2,17 @@
 for _ in range(int(input())):
     n=int(input())
     a=list(map(int,input().split()))
-    
+    oc=0
+    ec=0
+    for i in a:
+        if i%2==0:
+            ec=0
+        else:
+            ec=0
+    if oc==ec:
+        print(n)
+    else:
+        if oc%2!=0:
+            print(oc+2)
+        elif ev%2==0:
+            print(ec+2)
