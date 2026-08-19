45c11e600979acda880169924c0c75389355aa7c
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-19 22:07:12 +0530

    ✅ UPDQS | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/UPDQS/solution.py
+++ codechef/medium/UPDQS/solution.py
@@ -1,4 +1,8 @@
 # cook your dish here
 for _ in range(int(input())):
     n,q=map(int,input().split())
-    
+    a=list(map(int,input().split()))
+    i=[]
+    x=[]
+    for m in range(q):
+        
