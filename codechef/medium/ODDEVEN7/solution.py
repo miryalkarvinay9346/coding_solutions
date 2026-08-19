4a9182493951ed2a3240af7a1257e20367bb51bb
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-19 21:26:40 +0530

    ✅ ODDEVEN7 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/ODDEVEN7/solution.py
+++ codechef/medium/ODDEVEN7/solution.py
@@ -9,10 +9,4 @@ for _ in range(int(input())):
             ec=0
         else:
             ec=0
-    if oc==ec:
-        print(n)
-    else:
-        if oc%2!=0:
-            print(oc+2)
-        elif ev%2==0:
-            print(ec+2)
+    
