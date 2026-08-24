3afe3b62a4f049db01de7f1cc384391ef95510b5
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-24 19:28:17 +0530

    ✅ MNFLP | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/MNFLP/solution.py
+++ codechef/medium/MNFLP/solution.py
@@ -6,4 +6,9 @@ for _ in range(int(input())):
     if sum(a)==0:
         print(c)
     else:
-        
+        if n%2==1:
+            print(-1)
+        else:
+            c1=a.count(1)
+            c0=n//2
+            print(c1-c0)
