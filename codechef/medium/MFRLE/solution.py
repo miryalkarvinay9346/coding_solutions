2c215b27b8e8a392fd6ccc44800cb48c32540b33
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-24 19:39:09 +0530

    ✅ MFRLE | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/MFRLE/solution.py
+++ codechef/medium/MFRLE/solution.py
@@ -1,3 +1,9 @@
 # cook your dish here
 s=input().lower()
-
+a=[]
+for i in range(len(s)):
+    c=0
+    for j inn range(len(s)):
+        if s[i]==s[j] and s[i].isalpha():
+            c+=1
+    a.append(c)
