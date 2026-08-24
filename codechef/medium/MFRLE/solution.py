9b8b595fc48cb8072e6590e936bd4321c8778237
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-24 21:32:06 +0530

    ✅ MFRLE | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/MFRLE/solution.py
+++ codechef/medium/MFRLE/solution.py
@@ -1,3 +1,13 @@
 # cook your dish here
-s=input().lower()
-
+k=input().lower()
+s=''.join(sorted(k))
+a=[]
+for i in range(len(s)):
+    c=0
+    for j in range(len(s)):
+        if s[i]==s[j] and s[i].isalpha():
+            c+=1
+    a.append(c)
+m=max(a)
+ind=a.index(m)
+print(s[ind])
