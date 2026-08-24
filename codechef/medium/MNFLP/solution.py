ff415ca7bfe4c78d781519320f183f6b657a9b92
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-24 19:35:42 +0530

    ✅ MNFLP | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/MNFLP/solution.py
+++ codechef/medium/MNFLP/solution.py
@@ -3,12 +3,4 @@ for _ in range(int(input())):
     n=int(input())
     a=list(map(int,input().split()))
     c=0
-    if sum(a)==0:
-        print(c)
-    else:
-        if n%2==1:
-            print(-1)
-        else:
-            c1=a.count(1)
-            c0=n//2
-            print(c1-c0)
+    
