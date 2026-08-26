d3acea0a83f3426bf72311612cee05c80dd7ca5f
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-26 22:32:16 +0530

    ✅ DIFST | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/DIFST/solution.py
+++ codechef/medium/DIFST/solution.py
@@ -2,9 +2,12 @@
 for _ in range(int(input())):
     n=int(input())
     p=list(map(int,input().split()))
+    s=0
     for i in range(n):
         for j in range(n):
             if abs(p[i]-p[j])>=abs(i-j):
                 k=p[i]
                 p[i]=p[j]
                 p[j]=k
+                s+=1
+                
