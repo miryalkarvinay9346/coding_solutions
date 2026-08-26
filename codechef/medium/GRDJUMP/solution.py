879fbdc9ca77fa9419e80f41d602a062ba9560bb
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-26 21:34:55 +0530

    ✅ GRDJUMP | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/GRDJUMP/solution.py
+++ codechef/medium/GRDJUMP/solution.py
@@ -1,8 +1,4 @@
 # cook your dish here
 for _ in range(int(input())):
     a,b,p,q,r=map(int,input().split())
-    if a==b:
-        print(r*a)
-    else:
-        c=p*a+(q*b)
-        print(c)
+    
