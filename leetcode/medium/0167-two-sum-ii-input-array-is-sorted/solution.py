fe8d1db4ae63ba84873aff8e6f98cfc602e21901
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-26 16:05:01 +0530

    ✅ Two Sum II - Input Array Is Sorted | Medium | Python | Runtime: 3 ms (79.74%) | Memory: 20.5 MB | 2026-08-26

--- leetcode/medium/0167-two-sum-ii-input-array-is-sorted/solution.py
+++ leetcode/medium/0167-two-sum-ii-input-array-is-sorted/solution.py
@@ -1,5 +1,7 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        """
+        --- this causes TLE ERROR ,due to time complexity is O(n^2) ---
         a=[0,0]
         for i in range(len(numbers)):
             a[0]=i+1
@@ -7,3 +9,14 @@ class Solution:
                 if numbers[i]+numbers[j]==target:
                     a[1]=j+1
                     return a
+            """
+        l=0
+        r=len(numbers)-1
+        while l<r:
+            total=numbers[l]+numbers[r]
+            if total==target:
+                return[l+1, r+1]
+            elif total < target:#you need a larger sum → move left right.
+                l+=1
+            else:#you need a smaller sum → move right left.
+                r-= 1
