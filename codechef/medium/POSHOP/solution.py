68bc34827303c10bb878572c4ddc460c26ea259c
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-26 22:13:14 +0530

    ✅ POSHOP | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/POSHOP/solution.py
+++ codechef/medium/POSHOP/solution.py
@@ -2,13 +2,12 @@
 for _ in range(int(input())):
     n=int(input())
     c=list(map(int,input().split()))
-    a=0
+    a=max(c)
     for i in range(n):
-        
-        for j in range(n):
+        for j in range(i+1,n):
             if c[i]<=c[j] and c[j] >=c[i]:
                 k=c[i]+c[j]
-                a=max(k,max(c))
+                a=max(k,a)
     print(a)
                 
                 
