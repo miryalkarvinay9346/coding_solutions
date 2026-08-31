400d82c3363bec2cf89a8a0ef40413f67f37d07a
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-31 19:30:35 +0530

    ✅ SCOCN | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/SCOCN/solution.py
+++ codechef/medium/SCOCN/solution.py
@@ -2,18 +2,5 @@
 n=int(input())
 a=list(map(int,input().split()))
 x=int(input())
-if x not in a:
-    print(-1)
-elif a.count(x)==1:
-    print(-2)
-else:
-    k=0
-    m=0
-    for i in range(n):
-        for j in range(n):
-            if a[j]==x:
-                k+=1
-                m=j
-            if k==2:
-                print(m)
-                break
+k=a.count(x)
+print(-1 if k==0 else (-2 if k==1 else ))
