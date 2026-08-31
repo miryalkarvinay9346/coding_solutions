eb15e879d98dadc8092556837caf7e884bbc24ad
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-31 22:10:31 +0530

    ✅ LISH | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/LISH/solution.py
+++ codechef/medium/LISH/solution.py
@@ -2,22 +2,19 @@
 w=int(input())
 n=int(input())
 a=list(map(int,input().split()))
-if 2*sum(a)<w:
+if 2*sum(a)<w:3
     print(-1)
 else:
     a.sort(reverse=True)
     c=0
     m=w
-    k=1
     for i in range(n):
         for j in range(n):
             if m<=0:
-                k=1
                 break
-            else:
-                m=m-a[i]
-                c+=1
-        if k:
+            m=m-a[i]
+            c+=1
+        if m<=0:
             break
     print(c)
     
