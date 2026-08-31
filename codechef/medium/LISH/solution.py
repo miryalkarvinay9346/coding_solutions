a5139436043e997aad555d7eaa8dcb1b16da7f1f
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-31 19:50:31 +0530

    ✅ LISH | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/LISH/solution.py
+++ codechef/medium/LISH/solution.py
@@ -2,8 +2,16 @@
 w=int(input())
 n=int(input())
 a=list(map(int,input().split()))
-a.sort()
 if 2*sum(a)<20:
     print(-1)
 else:
-    
+    a.sort(reverse=True)
+    c=0
+    m=w
+    for i in range(n):
+        k=w%a[i]
+        if k>0 :
+            c+=(w//a[i])
+            m=m-(w//a[i])*a[i]
+    print(c)
+    #print(25//10)
