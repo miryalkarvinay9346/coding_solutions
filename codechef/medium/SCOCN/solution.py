e146a5dfe9d36c68bece5d65a1cdc57685785286
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-08-31 19:22:42 +0530

    ✅ SCOCN | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/SCOCN/solution.py
+++ codechef/medium/SCOCN/solution.py
@@ -11,9 +11,10 @@ else:
     m=0
     for i in range(n):
         for j in range(n):
-            if a[i]==a[j]:
+            if a[i]==x:
                 k+=1
                 m=j
         if k==2:
             print(m)
             break
+        m=0
