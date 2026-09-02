01f6c96f90b8c33f6153675d9674d1f778cf760b
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-02 21:28:10 +0530

    ✅ MAXSUM77 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/MAXSUM77/solution.py
+++ codechef/medium/MAXSUM77/solution.py
@@ -6,7 +6,7 @@ for _ in range(int(input())):
     i=0 
     j=n-1
     c=0
-    while(i<j):
+    while  (i<=j):# and c<k:
         #c=0
         l=a[i]
         r=a[j]
@@ -16,8 +16,8 @@ for _ in range(int(input())):
         else:
             s=s-l
             i=i+1
-        c+=1
-        if c==k:
-            break
+        #c+=1
+        #if c==k:
+         #   break
     print(s)
         
