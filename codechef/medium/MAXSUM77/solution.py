4be9ac6fb578ff7a0087fdad5e44ce3859c92212
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-02 20:12:46 +0530

    ✅ MAXSUM77 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/MAXSUM77/solution.py
+++ codechef/medium/MAXSUM77/solution.py
@@ -2,4 +2,21 @@
 for _ in range(int(input())):
     n,k=map(int,input().split())
     a=list(map(int,input().split()))
-    
+    s=sum(a)
+    i=0 
+    j=len(a)-1
+    c=0
+    while(i<=j):
+        l=a[i]
+        r=a[j]
+        if l>r:
+            s=s-r
+            j=j-1
+        else:
+            s=s-l
+            i=i+1
+        c+=1
+        if c==k:
+            break
+    print(s)
+        
