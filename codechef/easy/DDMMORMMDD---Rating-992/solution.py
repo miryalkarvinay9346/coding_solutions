5c548a6e192853ec81b2fb15684ac2c667b01bab
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-06 19:50:49 +0530

    ✅ DDMMORMMDD - Rating 992 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-06

--- codechef/easy/DDMMORMMDD---Rating-992/solution.py
+++ codechef/easy/DDMMORMMDD---Rating-992/solution.py
@@ -2,13 +2,12 @@ t = int(input())
 while t > 0:
     s = input()
     # Your code goes here
-    x=int(s[:2])
-    y=int(s[3:5])
-    if (x>=1 and x<=12) and (y>=1 and y<=12):
+    d=int(s[:2])
+    m=int(s[3:5])
+    if (d>=1 and d<=12) and (m>=1 and m<=12):
         print("BOTH")
+    elif d>=1 and d<=12 :
+        print("MM/DD/YYYY")
     else:
-        if x>=1 and x<=12:
-            print("MM/DD/YYYY")
-        else:
-            print("DD/MM/YYYY")
+        print("DD/MM/YYYY")
     t -= 1
