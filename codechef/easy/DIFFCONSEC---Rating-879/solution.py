b1fe0e4cca8a66ee149b31c0117d47ec7ec70cdf
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-06 19:41:49 +0530

    ✅ DIFFCONSEC - Rating 879 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-06

--- codechef/easy/DIFFCONSEC---Rating-879/solution.py
+++ codechef/easy/DIFFCONSEC---Rating-879/solution.py
@@ -2,13 +2,5 @@
 for _ in range(int(input())):
     n=int(input())
     s=input()
-    c=0
-    #c=s.count("00")+s.count("11")
-    for i in range(n-1):
-        if s[i]==s[i-1]:
-            c+=1
-    """
-    c=sum(1 for i in range(n - 1) if s[i] == s[i + 1])
-    print(c)
-    """
+    c=s.count("00")+s.count("11")
     print(c)
