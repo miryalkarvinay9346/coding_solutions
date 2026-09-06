d2acb38f574090bc33e624c104cb2bc6ecd15e9e
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-06 19:39:32 +0530

    ✅ ADDONE - Rating 940 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-06

--- codechef/easy/ADDONE---Rating-940/solution.py
+++ codechef/easy/ADDONE---Rating-940/solution.py
@@ -1,4 +1,14 @@
 # cook your dish here
 for _ in range(int(input())):
-    n=(input())
-    print(str(int(n)+1))
+    s=list(input())
+    c=0
+    #print(str(int(n)+1))
+    for i in range(len(s)-1,-1,-1):
+        if s[i]!="9":
+            s[i]=str(int(s[i])+1)
+            print("".join(s))
+            c=1
+            break
+        s[i]="0"
+    if not c:
+        print("1"+"".join(s))
