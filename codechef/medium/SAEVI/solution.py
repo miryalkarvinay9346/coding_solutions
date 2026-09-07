ffffd1f76fb1039cccb99ae2cb08bead8c8aee94
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-07 21:25:06 +0530

    ✅ SAEVI | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/SAEVI/solution.py
+++ codechef/medium/SAEVI/solution.py
@@ -1,5 +1,9 @@
 # cook your dish here
 n,k=map(int,input().split())
 a=list(map(int,input().split()))
-for a in nums:
-    if 
+s=0
+for i in range(n):
+    if i%2==0 and a[i]>2*k:
+        s+=a[i]
+print(s)
+        
