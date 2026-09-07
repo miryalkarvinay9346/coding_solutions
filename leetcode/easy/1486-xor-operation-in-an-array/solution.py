bc87e5af439e7038649e61e4c287b824b31c084f
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-07 22:12:04 +0530

    ✅ XOR Operation in an Array | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 19.2 MB | 2026-09-07

---
+++ leetcode/easy/1486-xor-operation-in-an-array/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        nums=[0]*n
+        s=0
+        for i in range(n):
+            nums[i]=start+2*i
+            s=s^nums[i]
+        return s
