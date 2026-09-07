6703be0c2fcd59d8f7a231882669e227f25912bb
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-07 21:41:04 +0530

    ✅ RSFN | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/RSFN/solution.py
+++ codechef/medium/RSFN/solution.py
@@ -1,7 +1,5 @@
 # cook your dish here
 n,q=map(int,input().split())
 i=list(map(int,input().split()))
-for _ in range(q):
-    l,r=map(int,input().split())
-    a=[]
-    print(sum(i[l-1:r]),i)
+l,r=map(int,input().split())
+a=[]
