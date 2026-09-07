d123e4f8b736c3fd079653331a3c52d71a38861d
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-07 21:39:04 +0530

    ✅ RSFN | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/RSFN/solution.py
+++ codechef/medium/RSFN/solution.py
@@ -1,4 +1,7 @@
 # cook your dish here
 n,q=map(int,input().split())
 i=list(map(int,input().split()))
-l,r=map(int,input().split())
+for _ in range(q):
+    l,r=map(int,input().split())
+    a=[]
+    
