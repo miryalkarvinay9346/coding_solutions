de55632448b67ab5b8a5667d77ba3412f94370bd
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-09 21:07:18 +0530

    ✅ UNQSHF | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/UNQSHF/solution.py
+++ codechef/medium/UNQSHF/solution.py
@@ -3,4 +3,4 @@ for _ in range(int(input())):
     n=int(input())
     a=input()
     b=input()
-    print("YES" if a.count("a")+b.count("b")==len(a) else "NO")
+    print("YES" if a.count("a")+b.count("a")==len(a) or a.count("b")+b.count("b")==len(b) else "NO")
