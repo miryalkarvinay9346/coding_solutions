9dbc98ea821744667c93ad48459617eed935913b
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-14 21:08:42 +0530

    ✅ BSSEA | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/BSSEA/solution.py
+++ codechef/medium/BSSEA/solution.py
@@ -1,21 +1,9 @@
 # cook your dish here
 n=int(input())
 a=list(map(int,input().split()))
-c=(min(a)+max(a))//2
-"""
+m=(a[0]+a[-1])//2
 if m==a[n//2]:
     print(m)
 else:
     k=min(a[n//2+1],a[(n//2)-1])
     print(k)
-"""
-t=a[0]
-for i in a:
-    d=abs(i-c)
-    k=abs(t-c)
-    if d<k:
-        t=i
-    elif d==k:
-        if i<t:
-            t=i
-print(t)
