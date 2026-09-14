aae1cae73f119b1f5169edc157b07c9a3f879c22
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-14 21:31:08 +0530

    ✅ MISNUMM | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/MISNUMM/solution.py
+++ codechef/medium/MISNUMM/solution.py
@@ -3,3 +3,15 @@ n=int(input())
 a=list(map(int,input().split()))
 m=int(input())
 b=list(map(int,input().split()))
+k=[]
+for i in range(m):
+    o=b.count(b[i])
+    g=a.count(b[i])
+    if o==g:
+        continue
+    else:
+        d=o-g
+        for _ in range(d):
+            k.append(b[i])
+k.sort()
+print(k)
