58a273e8c27c52716d477b06f9e0d20bdc0560ad
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-14 22:01:06 +0530

    ✅ TCTQU | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/TCTQU/solution.py
+++ codechef/medium/TCTQU/solution.py
@@ -1,3 +1,17 @@
 # cook your dish here
 n,k=map(int,input().split())
 t=list(map(int,input().split()))
+a=t[k]
+c=0
+for i  in range(n):
+    if i<=k:
+        if t[i]<a:
+            c=c+t[i]
+        else:
+            c=c+a
+    else:
+        if t[i]<a:
+            c=c+t[i]
+        else:
+            c=c+a
+print(c)
