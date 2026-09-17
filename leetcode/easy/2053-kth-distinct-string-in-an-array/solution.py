fd7cbcda38512c57daccfa00a43aff5e437cfd23
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-17 20:00:16 +0530

    ✅ Kth Distinct String in an Array | Easy | Python | Runtime: 0 ms | Memory: 19.2 MB | 2026-09-17

--- leetcode/easy/2053-kth-distinct-string-in-an-array/solution.py
+++ leetcode/easy/2053-kth-distinct-string-in-an-array/solution.py
@@ -5,7 +5,7 @@ class Solution:
             v=arr.count(arr[i])
             if v==1:
                 if v not in a:
-                    a.append(v)
+                    a.append(arr[i])
         """if k<len(a):
             return a[k-1]
         else:
