9709ec05b7788e8546893faef421f09e84868724
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-17 18:49:58 +0530

    ✅ ACS - Rating 739 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-17

--- codechef/easy/ACS---Rating-739/solution.py
+++ codechef/easy/ACS---Rating-739/solution.py
@@ -2,7 +2,7 @@
 # cook your dish here
 for _ in range(int(input())):
     a=int(input())
-    if a//100+a%100<=10:
+    if (a//100)+(a%100)<=10:
         print((a//100)+a%10)
     else:
         print(-1)
