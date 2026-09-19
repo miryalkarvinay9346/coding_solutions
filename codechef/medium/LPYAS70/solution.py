a7c2dac7735c269ac8d5fc686473b4641177e550
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-19 22:27:32 +0530

    ✅ LPYAS70 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-19

--- codechef/medium/LPYAS70/solution.py
+++ codechef/medium/LPYAS70/solution.py
@@ -1,8 +1,8 @@
 # cook your dish here
 a,b,c=map(int,input().split())
-if a>b and b>c:
+if a<b and b<c:
     print("Increasing")
-elif a<b and b<c:
+elif a>b and b>c:
     print("Decreasing")
 else:
     print("Neither")
