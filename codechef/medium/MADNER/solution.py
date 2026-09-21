50c789ae366d2805094bffeb11be739e793f519b
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-21 19:26:38 +0530

    ✅ MADNER | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/MADNER/solution.py
+++ codechef/medium/MADNER/solution.py
@@ -3,15 +3,20 @@ class Solution:
         # write your code here
         m=0
         c=0
+        j=0
         #k=students[0]
         for i in range(len(students)-1):
+            if j==1:#for skipping iteration 
+                j=0
+                continue
             k=students[i]
             if (k=="x" and students[i+1]=="y") or (k=="y" and students[i+1]=="x") :
-                c+=1
+                c=1
             else:
                 c=0
                 #k=students[i]
             if c==1:
                 m+=1
                 i=i+2
+                j=1
         return m
