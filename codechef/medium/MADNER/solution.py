10a5f610c5f310425a2e72395c80a3408c286f5b
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-21 19:20:48 +0530

    ✅ MADNER | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/MADNER/solution.py
+++ codechef/medium/MADNER/solution.py
@@ -3,14 +3,15 @@ class Solution:
         # write your code here
         m=0
         c=0
-        k=students[0]
+        #k=students[0]
         for i in range(len(students)-1):
-            if (k=="x" and students[i+1]=="y") or (k=="y" and students[i]=="x") :
+            k=students[i]
+            if (k=="x" and students[i+1]=="y") or (k=="y" and students[i+1]=="x") :
                 c+=1
-                
             else:
                 c=0
                 k=students[i]
             if c==1:
                 m+=1
+                i=i+2
         return m
