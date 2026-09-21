bcbbc5c6ed46e099b23a266a22fe117573fee7b4
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-21 19:44:31 +0530

    ✅ COTOG | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/COTOG/solution.py
+++ codechef/medium/COTOG/solution.py
@@ -1,1 +1,11 @@
 # cook your dish here
+n=int(input())
+a=list(map(int,input().split()))
+c=0
+for i in range(n-1):
+    k=0
+    if (a[i]==2*i-2 and a[i+1]==2*i) or (a[i]==2*n and a[i+1]==2*i-2):
+        continue
+    else:
+        c+=1
+print(c)
