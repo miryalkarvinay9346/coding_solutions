78bae701873003a1cb1d0b066eb9fddac4053457
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-21 19:47:43 +0530

    ✅ COTOG | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/COTOG/solution.py
+++ codechef/medium/COTOG/solution.py
@@ -4,7 +4,7 @@ a=list(map(int,input().split()))
 c=0
 for i in range(n-1):
     k=0
-    if (a[i]==2*i-2 and a[i+1]==2*i) or (a[i]==2*n and a[i+1]==2*i-2):
+    if a[i]==2*i-2 and a[i+1]==2*i:
         continue
     else:
         c+=1
