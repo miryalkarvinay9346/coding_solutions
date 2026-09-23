61993079cee6626146b2b8269a61a93180698243
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-23 22:06:15 +0530

    ✅ LITUP | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/LITUP/solution.py
+++ codechef/medium/LITUP/solution.py
@@ -2,20 +2,4 @@
 for _ in range(int(input())):
     n,k=map(int,input().split())
     c=list(map(int,input().split()))
-    a=100*100
-    for i  in range(n):
-        for j in range(i+1,n):
-            if (i+1)-k>1:
-                continue
-            if (j+1)+k<n:
-                print("J")
-                continue
-            else:
-                if (j+1)+k>i+1-k+1:
-                    print("K")
-                    continue
-            a=min(a,c[i]+c[j])
-    if a==100*100:
-        print(-1)
-    else:
-        print(a)
+    
