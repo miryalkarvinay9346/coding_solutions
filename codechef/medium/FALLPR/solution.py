a5877df5cac47757293c84f1bf1efd47e840101b
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-23 21:53:19 +0530

    ✅ FALLPR | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/FALLPR/solution.py
+++ codechef/medium/FALLPR/solution.py
@@ -2,9 +2,16 @@
 for _ in range(int(input())):
     n=int(input())
     a=list(map(int,input().split()))
+    cn=0
+    cp=0
+    """
+    if sum(a)>=0:
+        #print("YES")
+    else:"""
+    s=sum(a)
     c=0
     for i in range(n):
-        if sum(a[:i])+sum(a[i+1:])>=0:
+        if s+a[i]>=0:
             c=1
             break
     print("YES" if c else "NO")
