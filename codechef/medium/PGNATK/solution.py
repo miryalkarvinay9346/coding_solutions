648ce927c77559b4f7cab0bd4124d162feecc439
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-23 20:56:09 +0530

    ✅ PGNATK | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/PGNATK/solution.py
+++ codechef/medium/PGNATK/solution.py
@@ -1,4 +1,15 @@
 # cook your dish here
 for _ in range(int(input())):
     n,k=map(int,input().split())
-    
+    c=0
+    for i in range(1,max(n,k*n)):
+        if n==i*k:
+            c+=1
+        else:
+            c+=1
+            n=n-1
+        if n==0 :
+            print(c)
+            break
+        
+        
