a0454caefdfc0cc60ed2ffaa8a5dd06934a49337
miryalkarvinay9346 <miryalkarvinay12@gmail.com>
2026-09-23 10:17:33 +0530

    ✅ PEAKINARRAY1 - Rating 950 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/easy/PEAKINARRAY1---Rating-950/solution.py
@@ -0,0 +1,16 @@
+def findPeaks(A: list[int], n: int) -> list[int]:
+    # write your code here 
+    a=[]
+    c=1
+    p=A[0]
+    for i in range(len(A)):
+        if i==0:
+            if p>A[i+1]:
+                a.append(p)
+        elif i==len(A)-1:
+            if A[i]>A[i-1]:
+                a.append(A[i])
+        else:
+            if A[i]>A[i-1] and i<len(A) and A[i]>A[i+1] :
+                a.append(A[i])
+    return a if len(a)>0 else -1
