472c94ef2f1eabe640a45632da1ee6a62fb48e79
Miryalkar Vinay <miryalkarvinay12@gmail.com>
2026-09-26 21:14:13 +0530

    ✅ Q1. Minimum Queen Moves to Reach Target | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 19.4 MB | 2026-09-26

---
+++ leetcode/easy/4421-minimum-queen-moves-to-reach-target/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minQueenMoves(self, source: list[int], target: list[int]) -> int:
+        if source==target:
+            return 0
+        if source[0]==target[0] or source[1]==target[1] or abs(source[0]-target[0])==abs(source[1]-target[1]):
+            return 1
+        return 2
